5df75d8f3853bec0d7f0eb214fe4e9fc2174578c
jshep23prog <jshep23@gmail.com>
2024-08-01 13:06:06 -0500

    added operands and changed count and counter vars for readability

--- practice.py
+++ practice.py
@@ -6,21 +6,40 @@ import ast
 root = Tk()
 root.title("James' blind attempt")
 
+#write function that adds text to entry field
+i = 0
+def get_number(num):
+  global i
+  display.insert(i, num) #add to entry widget
+  i += 1 #increment every button click
+
 
 #entry field and grid display
 display = Entry(root)
 display.grid(row = 1, columnspan = 6)
 
 #add buttons 1- 9 with nested for loop
-counter = 1
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+num_count = 0
 for x in range(3):
   for y in range(3):
-    button = Button(text = counter, width = 2, height = 2)
+    button_text = numbers[num_count]
+    button = Button(root, text = button_text, width = 2, height = 2)
     button.grid(row = x + 2, column = y)
-    counter += 1
+    num_count += 1
 
 #need a zero button outside loop
-button = Button(text = 0, width = 2, height = 2)
+button = Button(root, text = "0", width = 2, height = 2)
 button.grid(row = 5, column = 1)
+
+#add operands
+op_count = 0
+operations = ["+", "-", "*", "/", "*3.14", "%", "(", "**", ")", "**2"]
+for x in range(4):
+  for y in range(3):
+    if op_count < len(operations):
+      button = Button(root, text = operations[op_count], width = 2, height = 2)
+      op_count += 1
+      button.grid(row = x + 2, column = y + 3)
 #keep window running
 root.mainloop()
