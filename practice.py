c69263886d9fecc5185caece316c90f0fdfc42ea
jshep23prog <jshep23@gmail.com>
2024-08-01 13:13:26 -0500

    added operands function and lambdas to get them to display

--- practice.py
+++ practice.py
@@ -13,6 +13,12 @@ def get_number(num):
   display.insert(i, num) #add to entry widget
   i += 1 #increment every button click
 
+#write a function to get operands
+def get_operands(op):
+  global i
+  length = len(op) #to handle multi digit ops like pi
+  display.insert(i, op)
+  i += length
 
 #entry field and grid display
 display = Entry(root)
@@ -24,12 +30,12 @@ num_count = 0
 for x in range(3):
   for y in range(3):
     button_text = numbers[num_count]
-    button = Button(root, text = button_text, width = 2, height = 2)
+    button = Button(root, text = button_text, width = 2, height = 2, command = lambda text = button_text: get_number(text)) #add lambda function to call get_num
     button.grid(row = x + 2, column = y)
     num_count += 1
 
 #need a zero button outside loop
-button = Button(root, text = "0", width = 2, height = 2)
+button = Button(root, text = "0", width = 2, height = 2, command = lambda text = button_text: get_number(0))
 button.grid(row = 5, column = 1)
 
 #add operands
@@ -38,7 +44,7 @@ operations = ["+", "-", "*", "/", "*3.14", "%", "(", "**", ")", "**2"]
 for x in range(4):
   for y in range(3):
     if op_count < len(operations):
-      button = Button(root, text = operations[op_count], width = 2, height = 2)
+      button = Button(root, text = operations[op_count], width = 2, height = 2, command = lambda text = operations[op_count]: get_operands(text))
       op_count += 1
       button.grid(row = x + 2, column = y + 3)
 #keep window running
